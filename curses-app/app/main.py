df24c20975e8435e5e2f2e3ba413e9b38201c48f
prehensile <me@prehensile.co.uk>
2018-10-21 22:17:23 +0100

    use my own menu drawing & art loading code; remove curses-menu dependency

--- curses-app/app/main.py
+++ curses-app/app/main.py
@@ -1,46 +1,89 @@
 # Import the necessary packages
-from cursesmenu import *
-from cursesmenu.items import *
-
-banner = None
-with open( "banner.txt" ) as fp:
-  banner = fp.read()
-
-# Create the menu
-# menu = CursesMenu( "S P A C E W I Z A R D S", "Subtitle", banner=banner)
-menu = CursesMenu( banner=banner ) 
+import curses
 
 
-# Create some items
+def load_art( fn ):
+    lines = None
+    with open( fn ) as fp:
+        lines = fp.readlines()
+    return [ l.strip("\r\n") for l in lines ]
 
-# MenuItem is the base class for all items, it doesn't do anything when selected
-# menu_item = MenuItem("Menu Item")
 
-# # A FunctionItem runs a Python function when selected
-# function_item = FunctionItem("Call a Python function", input, ["Enter an input"])
+art_root = "./art"
 
-# # A CommandItem runs a console command
-# command_item = CommandItem("Run a console command",  "touch hello.txt")
+banner = load_art( "%s/banner.txt" % art_root )
 
-# # A SelectionMenu constructs a menu from a list of strings
-# selection_menu = SelectionMenu(["item1", "item2", "item3"])
+menu_files = [ "about.txt", "work.txt", "contact.txt" ]
+menu_arts = []
+for mf in menu_files:
+    menu_arts.append( 
+        load_art( "%s/%s" % (art_root,mf) )
+    )
 
-# # A SubmenuItem lets you add a menu (the selection_menu above, for example)
-# # as a submenu of another menu
-# submenu_item = SubmenuItem("Submenu item", selection_menu, menu)
 
-# # Once we're done creating them, we just add the items to the menu
-# menu.append_item(menu_item)
-# menu.append_item(function_item)
-# menu.append_item(command_item)
-# menu.append_item(submenu_item)
+def put_art( screen, y, x, lines, attr ):
+    i = 0
+    for line in lines:
+        screen.addstr( y+i, x, line, attr )
+        i += 1
+    return i
 
-items = [ "Projects", "About us", "Contact" ]
+def draw_highlight( screen, top, bottom, attr ):
+    w = screen.getmaxyx()[1]
+    line = '{0: <{fill}}'.format('', fill=w)
+    for row in range(top,bottom):
+        screen.addstr( row, 0, line, attr )
 
-for item in items:
-	menu.append_item(
-		MenuItem( item )
-	)
 
-# Finally, we call show to show the menu and allow the user to interact
-menu.show()
+
+def draw( screen, selected_art ):
+    # Clear screen
+    screen.clear()
+
+    row = 2
+    col = 8
+
+    row += put_art( screen, row, col, banner, curses.A_NORMAL )
+    row +=2
+
+    i = 0
+    for menu_art in menu_arts:
+        selected = (i==selected_art)
+        attr = curses.A_REVERSE if selected else curses.A_NORMAL 
+        if selected:
+            draw_highlight( screen, row, row+len(menu_art), attr )
+        row += put_art( screen, row, col, menu_art, attr )        
+        i += 1
+
+    screen.border()
+
+    screen.refresh()
+
+
+def main( screen ):
+
+    selected_art = 0
+    draw( screen, selected_art )
+    
+    while True:
+        
+        k = screen.getch(0,0)
+
+        if k == curses.KEY_UP:
+            selected_art -=1
+            if selected_art < 0:
+                selected_art = len( menu_arts)-1
+        
+        elif k == curses.KEY_DOWN:
+            selected_art += 1
+            if selected_art >= len( menu_arts ):
+                selected_art = 0
+
+        elif k == ord('q'):
+            break
+
+        draw( screen, selected_art )
+
+
+
+curses.wrapper( main )
